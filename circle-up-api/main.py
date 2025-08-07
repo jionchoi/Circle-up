0d8df799fa6363f2502c6a38c60b5cb6c7c8e9f2
Jion Choi <chlwldhs@gmail.com>
2025-08-07 10:47:11 -0600

    initial set up done

--- circle-up-api/main.py
+++ circle-up-api/main.py
@@ -5,9 +5,14 @@ from dotenv import load_dotenv
 #load environment variables
 load_dotenv()
 
+SUPABASE_URL = os.getenv('SUPABASE_URL')
+SUPABASE_KEY = os.getenv('SUPABASE_KEY')
 
+if not all (SUPABASE_URL, SUPABASE_KEY):
+    raise EnvironmentError("One or more Supabase environment variables are missing")
 
-supabase: Client = create_client(os.getenv('SUPABASE_URL'), os.getenv('SUPABASE_KEY'))
+#Connect supabase database
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 results = supabase.table('user').select('*').execute()
 print(results)
