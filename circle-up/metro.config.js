4783c25a0829332e8dc71e17a6a3ed66002aa8ae
Jion Choi <chlwldhs@gmail.com>
2025-08-07 12:31:34 -0600

    node modules removed

--- circle-up/metro.config.js
+++ circle-up/metro.config.js
@@ -4,18 +4,3 @@ const { withNativeWind } = require('nativewind/metro');
 const config = getDefaultConfig(__dirname)
  
 module.exports = withNativeWind(config, { input: './app/globals.css' })
-
-module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts }
-  } = await getDefaultConfig();
-  return {
-    transformer: {
-      babelTransformerPath: require.resolve("react-native-svg-transformer")
-    },
-    resolver: {
-      assetExts: assetExts.filter(ext => ext !== "svg"),
-      sourceExts: [...sourceExts, "svg"]
-    }
-  };
-})();
